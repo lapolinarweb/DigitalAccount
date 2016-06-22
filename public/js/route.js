3443eaa85aee8a8cd5531680fe8e23492dc2ba9d
Vignesh <vignesh328@gmail.com>
2016-06-22 09:22:26 +0530

    Signature Page- Commit

--- public/js/route.js
+++ public/js/route.js
@@ -4,15 +4,34 @@ var bankingApp = angular.module('bankingApp', ['ngRoute'])
                             $routeProvider
                                 //route for index page
                                 .when('/', {
-                                    templateUrl  : 'landingPage.html',
-                                    controller   : 'home',
-                                })
-
-                                // route for the home page
-                                .when('/home', {
-                                    templateUrl  : 'home.html',
+                                    templateUrl  : 'Homepage.html',
                                     controller   : 'home',
                                     controllerAs : 'homeController'
                                 })
+
+                                // route for the home page
+                                .when('/Services', {
+                                    templateUrl  : 'Services.html',
+                                    controller   : 'home',
+                                    controllerAs : 'homeController'
+                                })
+
+                                // route for the Product Details page
+                                .when('/ProductDetails', {
+                                    templateUrl  : 'ProductDetails.html',
+                                    controller   : 'home',
+                                    controllerAs : 'homeController'
+                                })
+
+                                // route for the Product Details page
+                                .when('/signature', {
+                                    templateUrl  : 'Signature_Screen.html',
+                                    controller   : 'home',
+                                    controllerAs : 'homeController'
+                                })
+                                .otherwise({
+                                  redirectTo : '/'
+                                })
+
 
                         });
